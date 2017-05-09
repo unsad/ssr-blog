761869d77c3a63b9bb2e843d2a0727417b90c296
unsad <wzxaylryw@hotmail.com>
2017-05-09 11:30:56 +0800

    Koa后端初始化

--- server_side/app.js
+++ server_side/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by unsad on 2017/5/9.
  */
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
+app.use(ctx => {
+  ctx.body = 'hello koa';
+});
+
+app.listen(3000);
+console.log('koa2 is running at 3000');

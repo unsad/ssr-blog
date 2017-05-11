cbbdb8036046f4acd36ffa21aebb5591e56f9324
unsad <wzxaylryw@hotmail.com>
2017-05-11 18:42:18 +0800

    [back]更新findAll的API接口

--- server_side/mongo_rest/actions.js
+++ server_side/mongo_rest/actions.js
@@ -1,3 +1,29 @@
 /**
  * Created by unsad on 2017/5/10.
  */
+module.exports = function generateActions(model) {
+  return {
+    findAll: async function(next) {
+      await next;
+      let error, result;
+      try {
+        let conditions = {};
+        let query = this.request.query;
+        if (query.conditions) {
+          conditions = JSON.parse(query.conditions);
+        }
+        let builder = model.find(conditions);
+        ['limit', 'skip', 'sort'].forEach(function(key) {
+          if (query[key]) {
+            builder[key](query[key]);
+          }
+        });
+        result = await builder.exec();
+        return this.body = result;
+      } catch (_error) {
+        error = _error;
+        return this.body = error;
+      }
+    }
+  }
+};

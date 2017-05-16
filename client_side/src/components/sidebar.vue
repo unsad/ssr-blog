b2e324c1b99f8a058c61af292b36a8c3d457b92c
unsad <wzxaylryw@hotmail.com>
2017-05-16 19:41:35 +0800

    [front]侧边栏组件初始化

---
+++ client_side/src/components/sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav id="sidebar">
+    <div class="profile">
+      <a href="/">这里是logo</a><span>这里是站点标题</span>
+    </div>
+    <ul class="buttons">
+      <li>首页</li>
+      <li>归档</li>
+      <li>标签</li>
+      <li>关于</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'sidebar'
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

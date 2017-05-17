3d25c0056de725d0eb8004d8c6e6d6bdc82dfee9
unsad <wzxaylryw@hotmail.com>
2017-05-17 20:57:48 +0800

    [front]stylus相关依赖安装，eslintrc配置更改

--- client_side/src/App.vue
+++ client_side/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <sidebar></sidebar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import sidebar from './components/sidebar.vue'
+  export default {
+    name: 'app',
+    components: {
+      sidebar
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>

32005512035cf21bda2fc408eb08826ce2bcc97b
unsad <wzxaylryw@hotmail.com>
2017-05-18 21:53:59 +0800

    [front]跨域代理设置

--- client_side/config/index.js
+++ client_side/config/index.js
@@ -27,7 +27,15 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/proxyPrefix': {
+        target: 'http//localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/proxyPrefix': ''
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)

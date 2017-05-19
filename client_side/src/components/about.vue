4dde6519f0fa43f0a420fcb741555aedc70746c9
unsad <wzxaylryw@hotmail.com>
2017-05-19 22:43:07 +0800

    [front]about页面搭建

---
+++ client_side/src/components/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{html}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'about',
+    data() {
+      return {
+        html: this.$http.get('/proxyPrefix/api/post/57dbe47c2993f70dc6d6b12c').then((response) => {
+          console.log('response ok');
+          this.html = response;
+        }, (err) => {
+          console.log('response error', err)
+        })
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

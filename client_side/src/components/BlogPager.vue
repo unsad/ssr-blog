b3780171b0ac7900b0e3b47bffe137729470c588
unsad <wzxaylryw@hotmail.com>
2017-05-23 14:51:46 +0800

    [front]初始化博客分页页面

---
+++ client_side/src/components/BlogPager.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main">
+    <section id="page-index">
+      <li v-for="item in items">
+        {{item.title}}
+      </li>
+    </section>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+  import store from '../store/index';
+  import myFooter from './Footer.vue';
+
+  export default {
+    name: 'BlogPager',
+    data() {
+      return {
+        items: store.fetchBlogByPage(this, 0).then(items => this.items = items)
+      }
+    },
+    components: {
+      myFooter
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

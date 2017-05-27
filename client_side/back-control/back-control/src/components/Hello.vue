9155afe75f7c5d74cd384a9dd676b0b2bf080007
unsad <wzxaylryw@hotmail.com>
2017-05-27 15:58:51 +0800

    [back-control]初始化login,tip组件

--- client_side/back-control/back-control/src/components/Hello.vue
+++ client_side/back-control/back-control/src/components/Hello.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <template v-if="isLogin === false">
+      <tip :should-tip-show="showTipShow" type="0" :text="placeholder"></tip>
+      <login></login>
+    </template>
   </div>
 </template>
 
 <script>
+import Login from './Login';
+import Tip from './Tip';
+
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isLogin: false,
+      shouldTipShow: false,
+      SystemInfo: {}
     }
+  },
+  components: {
+    Login,
+    Tip
   }
 }
 </script>
--- client_side/back-control/back-control/src/router/index.js
+++ client_side/back-control/back-control/src/router/index.js
@@ -2,14 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
-})
+});
+
+router.beforeEach(function() {
+  window.scrollTo(0, 0);
+});
+
+export default router;
+

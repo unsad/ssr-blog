1fa46fa91174e7f257f8db6c4360084a1c2b1892
unsad <wzxaylryw@hotmail.com>
2017-05-30 13:43:51 +0800

    [back-control]完成login组件

--- client_side/back-control/back-control/src/components/Login.vue
+++ client_side/back-control/back-control/src/components/Login.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="row">
+      <div class="login">
+        <h1 class="text-center">
+          <a href="/">{{title}}</a>
+        </h1>
+        <form>
+          <input type="text" name="username" ref="username" class="form-control" required pattern="\w{1, 15}"
+                 placeholder="用户名"
+          <input type="password" name="password" ref="password" class="form-control" required pattern="\w{1, 15}"
+                 placeholder="密码">
+          <button type="submit">登录</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'login'
+    name: 'login',
+    data() {
+      return {
+        title: '造梦之地'
+      }
+    }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">

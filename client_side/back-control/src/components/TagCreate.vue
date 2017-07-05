aa3540ccd408a4f03e9d25c2f867a2b931a5d79c
unsad <wzxaylryw@hotmail.com>
2017-07-05 21:27:36 +0800

    [back-control]初始化TagCreate,CateCreate,UserEdit的html结构

--- client_side/back-control/src/components/TagCreate.vue
+++ client_side/back-control/src/components/TagCreate.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-
+  <div class="wrap">
+    <top :current-route="currentRoute"></top>
+    <div class="manage-container">
+      <form>
+        <div class="form-group">
+          <label for="">标签名称</label>
+          <div>
+            <input type="text">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="">缩略名</label>
+          <div>
+            <input type="text">
+          </div>
+        </div>
+        <div class="form-group">
+          <button type="submit">{{submitting ? '提交中' : '提交'}}</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+  import Top from './Top'
+
   export default {
-    name: 'tagCreate'
+    name: 'tagCreate',
+    data() {
+      return {
+
+      }
+    },
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String,
+      currentRoute: String
+    }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
--- client_side/back-control/src/components/UserEdit.vue
+++ client_side/back-control/src/components/UserEdit.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-
+  <div class="wrap">
+    <top :current-route="currentRoute"></top>
+    <div class="manage-container">
+      <form>
+        <div class="form-group">
+          <label for="">密码</label>
+          <div>
+            <input type="password">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="">确认密码</label>
+          <div>
+            <input type="password">
+          </div>
+        </div>
+        <div class="form-group">
+          <button type="submit">{{submitting ? '提交中' : '提交'}}</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+  import Top from './Top'
+
   export default {
-    name: 'userEdit'
+    name: 'userEdit',
+    data() {
+      return {
+
+      }
+    },
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String,
+      currentRoute: String
+    }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">

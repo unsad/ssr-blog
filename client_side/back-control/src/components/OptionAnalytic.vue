cc4d748809fe8b56e0c7e8f61f8dac61470c22a4
unsad <wzxaylryw@hotmail.com>
2017-07-07 22:20:38 +0800

    [back-control]初始化评论/统计组件的html结构

---
+++ client_side/back-control/src/components/OptionAnalytic.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrap">
+    <top :current-route="currentRoute"></top>
+    <div class="manage-container">
+      <h3>网站统计代码</h3>
+      <form action="#" class="options-general">
+      <div class="form-group">
+        <div class="form-group">
+          <textarea name="analyze_code" cols="30" rows="10"></textarea>
+        </div>
+        <p>统计代码</p>
+      </div>
+      <button type="submit">提交</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Top from './Top'
+
+  export default {
+    name: 'optionAnalytic',
+    components: {
+      Top
+    },
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String,
+      currentRoute: String
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ client_side/back-control/src/components/OptionComment.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrap">
+    <top :current-route="currentRoute"></top>
+    <div class="manage-container">
+      <form action="#" class="options-comment">
+        <div class="form-group">
+          <label>评论类型</label>
+          <div class="form-group">
+            <div>
+              <div class="">
+                <div class="radio">
+                  <label for=""><input type="radio"><span>评论</span></label>
+                </div>
+              </div>
+              <div class="">
+                <div class="radio">
+                  <label for=""><input type="radio"><span>自定义</span></label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for=""><span>网站名称</span></label>
+            <div class="form-group"><input type="text" value="unsad"></div>
+          </div>
+          <button type="submit">
+            提交
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Top from './Top'
+
+  export default {
+    name: 'optionComment',
+    components: {
+      Top
+    },
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String,
+      currentRoute: String
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

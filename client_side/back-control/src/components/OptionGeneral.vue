7a397ff11640553866cc0e127be63eda4026f3ba
unsad <wzxaylryw@hotmail.com>
2017-07-08 22:17:16 +0800

    [back-control]初始化用户编辑/选项组件的html结构

--- client_side/back-control/src/components/OptionGeneral.vue
+++ client_side/back-control/src/components/OptionGeneral.vue
@@ -1,10 +1,71 @@
 <template>
-  <div>
-
+  <div class="wrap">
+    <top :current-route="currentRoute"></top>
+    <div class="manage-container">
+      <form action="#">
+        <div class="form-group">
+          <label for="">站点名称</label>
+          <div class="form-group">
+            <input type="text" name="title" value="unsad">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="">LOGO地址</label>
+          <div class="form-group">
+            <input type="text" name="logo_url">
+          </div>
+          <p>
+            <span>尺寸：140X140</span>
+            <span>上传</span>
+          </p>
+        </div>
+        <div class="form-group"><label for="">站点描述</label>
+          <div class="form-group">
+            <input type="text" name="description">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="">网站地址</label>
+          <div class="form-group">
+            <input type="text" name="site_url">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="">favicon地址</label>
+          <div class="form-group">
+            <input type="text" name="favicon_url">
+          </div>
+          <p><span>尺寸：128X128</span>
+            <span>上传</span>
+          </p>
+        </div>
+        <div class="form-group"><label for="">关键词</label>
+          <div class="form-group">
+            <input type="text" name="keywords">
+            <p>请以半角逗号,分隔关键字</p>
+          </div>
+        </div>
+          <div class="form-group">
+            <label for="">GitHub地址</label>
+            <div class="form-group">
+              <input type="text" name="github_url">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="">网站备案号</label>
+            <div class="form-group">
+              <input type="text" name="miitbeian">
+            </div>
+          </div>
+        <button type="submit">提交</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+  import Top from './Top'
+
   export default {
     name: 'optionGeneral'
   }
--- client_side/back-control/src/components/UserList.vue
+++ client_side/back-control/src/components/UserList.vue
@@ -1,10 +1,51 @@
 <template>
-  <div>
-
+  <div class="wrap">
+    <top :current-route="currentRoute">
+      <div class="manage-container">
+        <form action="">
+          <div class="left">
+            <div class="form-group">
+              <label for="">用户名</label>
+              <input type="text" placeholder="4到20个字符" validate="required">
+              <p>登录时所用名称，不能重复</p>
+            </div>
+            <div class="form-group">
+              <label for="">邮箱</label>
+              <input type="text" name="email" placeholder="4到20个字符" validate="required">
+              <p>用户主要联系方式，不能重复</p>
+            </div>
+            <div class="form-group">
+              <label for="">密码</label>
+              <input type="password" name="password">
+              <p>建议</p>
+            </div>
+            <div class="form-group">
+              <label for="">确认密码</label>
+              <input type="password" name="repassword" validate="required">
+              <button type="submit">{{submitting ? '提交中...' : '提交'}}</button>
+            </div>
+          </div>
+          <div class="left">
+            <div class="form-group">
+              <label for="">别名</label>
+              <input type="text" placeholder="显示名称" validate="required">
+              <p>登录时所用名称，不能重复</p>
+            </div>
+            <div class="form-group">
+              <label>用户名</label>
+              <input type="password" name="username" placeholder="4到20个字符">
+              <p>登录时所用名称，不能重复</p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </top>
   </div>
 </template>
 
 <script>
+  import Top from './Top'
+
   export default {
     name: 'userList'
   }

77cc97452dd83ddabfff943f34d740203e708a9d
unsad <wzxaylryw@hotmail.com>
2017-07-12 00:20:18 +0800

    [back-control]引入日历组件

--- client_side/back-control/src/components/PostCreate.vue
+++ client_side/back-control/src/components/PostCreate.vue
@@ -17,68 +17,7 @@
                 <input type="text" name="pathname"></div><span>.html</span>
             </div>
             <div class="form-group">
-              <div class="md-panel">
-                <div class="md-menubar">
-                  <ul class="md-modebar">
-                    <li>
-                      <a href="" title="预览模式"><i></i></a>
-                    </li>
-                    <span></span>
-                    <li>
-                      <a href="" title="分屏模式"><i></i></a>
-                    </li>
-                    <span></span>
-                    <li>
-                      <a href="" title="编辑模式"><i></i></a>
-                    </li>
-                    <span></span>
-                    <li>
-                      <a href="" title="全屏模式"><i></i></a>
-                    </li>
-                    <span></span>
-                  </ul>
-                  <ul class="md-toolbar">
-                    <li class="tb-btn">
-                      <a href="" title="加粗(Ctrl + B)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="斜体(Ctrl + I)"><i></i></a>
-                    </li>
-                    <li class="tb-btn spliter"></li>
-                    <li class="tb-btn">
-                      <a href="" title="链接(Ctrl + L)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="引用(Ctrl + Q)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="代码段(Ctrl + K)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="图片(Ctrl + G)"><i></i></a>
-                    </li>
-                    <li class="tb-btn spliter"></li>
-                    <li class="tb-btn">
-                      <a href="" title="有序列表(Ctrl + O)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="无序列表(Ctrl + U)"><i></i></a>
-                    </li>
-                    <li class="tb-btn">
-                      <a href="" title="标题(Ctrl + H)"><i></i></a>
-                    </li>
-                    <li class="tb-btn spliter"></li>
-                    <li class="tb-btn">
-                      <a href="" title="插入more标签(Ctrl + M)"><i></i></a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="md-editor">
-                  <textarea name="content" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div class="md-preview"></div>
-                <div class="md-spliter"></div>
-              </div>
+                <markdown-editor></markdown-editor>
               <p><span>文章使用markdown格式</span></p>
             </div>
           </div>
@@ -96,7 +35,7 @@
               <div>
                 <div class="rdt">
                   <input type="text">
-                  <div class="rdtPicker"></div>
+                  <date-picker></date-picker>
                 </div>
               </div></div>
           </div>
@@ -137,12 +76,17 @@
 </template>
 
 <script>
-  import Top from './Top'
+  import Top from './Top';
+  import MarkdownEditor from './editor/index';
+  import DatePicker from 'vue-datepicker';
+  import moment from 'moment';
 
   export default {
-    name: 'postCreate',
+    name: 'pageCreate',
     components: {
-      Top
+      Top,
+      MarkdownEditor,
+      DatePicker
     },
     props: {
       shouldTipShow: Boolean,
---
+++ client_side/back-control/src/components/editor/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="md-panel">
+    <div class="md-menubar">
+      <ul class="md-modebar">
+        <li>
+          <a href="" title="预览模式"><i></i></a>
+        </li>
+        <span></span>
+        <li>
+          <a href="" title="分屏模式"><i></i></a>
+        </li>
+        <span></span>
+        <li>
+          <a href="" title="编辑模式"><i></i></a>
+        </li>
+        <span></span>
+        <li>
+          <a href="" title="全屏模式"><i></i></a>
+        </li>
+        <span></span>
+      </ul>
+      <ul class="md-toolbar">
+        <li class="tb-btn">
+          <a href="" title="加粗(Ctrl + B)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="斜体(Ctrl + I)"><i></i></a>
+        </li>
+        <li class="tb-btn spliter"></li>
+        <li class="tb-btn">
+          <a href="" title="链接(Ctrl + L)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="引用(Ctrl + Q)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="代码段(Ctrl + K)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="图片(Ctrl + G)"><i></i></a>
+        </li>
+        <li class="tb-btn spliter"></li>
+        <li class="tb-btn">
+          <a href="" title="有序列表(Ctrl + O)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="无序列表(Ctrl + U)"><i></i></a>
+        </li>
+        <li class="tb-btn">
+          <a href="" title="标题(Ctrl + H)"><i></i></a>
+        </li>
+        <li class="tb-btn spliter"></li>
+        <li class="tb-btn">
+          <a href="" title="插入more标签(Ctrl + M)"><i></i></a>
+        </li>
+      </ul>
+    </div>
+    <div class="md-editor">
+      <textarea name="content" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div class="md-preview"></div>
+    <div class="md-spliter"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'editor',
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

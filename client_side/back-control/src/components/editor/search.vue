24b43c7ccb37e194ec003b618f94f83d21e685f1
unsad <wzxaylryw@hotmail.com>
2017-07-12 20:36:02 +0800

    [back-control]初始化搜索组件

---
+++ client_side/back-control/src/components/editor/search.vue
@@ -0,0 +1,23 @@
+<template>
+  <Select placeholder="请输入文章标题">
+
+  </Select>
+  <Option v-for="opt of options" >{{opt.title}}</Option>
+</template>
+
+<script>
+  export default {
+    name: 'search',
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String
+    },
+    data() {
+
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

93f2623184413a8372ffd769685cb6e62ba8822d
unsad <wzxaylryw@hotmail.com>
2017-07-21 01:09:21 +0800

    [back-control]完成目录组件

--- client_side/back-control/src/components/TagCreate.vue
+++ client_side/back-control/src/components/TagCreate.vue
@@ -2,21 +2,21 @@
   <div class="wrap">
     <top :current-route="currentRoute"></top>
     <div class="manage-container">
-      <form>
+      <form model="{tagInfo}" onsubmit="return false">
         <div class="form-group">
           <label for="">标签名称</label>
           <div>
-            <input type="text">
+            <input type="text" name="name" v-model="name" validate="required">
           </div>
         </div>
         <div class="form-group">
           <label for="">缩略名</label>
           <div>
-            <input type="text">
+            <input type="text" name="pathname" validate="required">
           </div>
         </div>
         <div class="form-group">
-          <button type="submit">{{submitting ? '提交中' : '提交'}}</button>
+          <button type="submit" @click="submitTag">{{isSubmitting ? '提交中' : '提交'}}</button>
         </div>
       </form>
     </div>
@@ -24,13 +24,24 @@
 </template>
 
 <script>
-  import Top from './Top'
+  import Top from './Top';
+  import store from '../store/index';
 
   export default {
     name: 'tagCreate',
     data() {
       return {
-
+        name: '',
+        isSubmitting: false
+      }
+    },
+    methods: {
+      submitTag() {
+        this.isSubmitting = true;
+        store.newTag(this, this.name).then(body => {
+          console.log('tagCreate', body);
+          this.isSubmitting = false;
+        });
       }
     },
     props: {

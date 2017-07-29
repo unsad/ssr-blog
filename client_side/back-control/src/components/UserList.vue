c77c61c2ddd3fb4843dc3e2d53de6012d375653b
unsad <wzxaylryw@hotmail.com>
2017-07-29 14:12:39 +0800

    [back-control]完成用户修改页面

--- client_side/back-control/src/components/UserList.vue
+++ client_side/back-control/src/components/UserList.vue
@@ -2,38 +2,33 @@
   <div class="wrap">
     <top :current-route="currentRoute">
       <div class="manage-container">
-        <form action="">
+        <form action="" onsubmit="return false">
           <div class="left">
             <div class="form-group">
               <label for="">用户名</label>
-              <input type="text" placeholder="4到20个字符" validate="required">
+              <input type="text" placeholder="4到20个字符" v-model="user.name" validate="required">
               <p>登录时所用名称，不能重复</p>
             </div>
             <div class="form-group">
               <label for="">邮箱</label>
-              <input type="text" name="email" placeholder="4到20个字符" validate="required">
+              <input type="text" v-model="user.email" name="email" placeholder="4到20个字符" validate="required">
               <p>用户主要联系方式，不能重复</p>
             </div>
             <div class="form-group">
               <label for="">密码</label>
-              <input type="password" name="password">
+              <input type="password" name="password" v-model="user.password" validate="required">
               <p>建议</p>
             </div>
             <div class="form-group">
               <label for="">确认密码</label>
-              <input type="password" name="repassword" validate="required">
-              <button type="submit">{{submitting ? '提交中...' : '提交'}}</button>
+              <input type="password" name="repassword" validate="required" v-model="repassword">
+              <button type="submit" @click="submit">{{submitting ? '提交中...' : '提交'}}</button>
             </div>
           </div>
           <div class="left">
             <div class="form-group">
               <label for="">别名</label>
-              <input type="text" placeholder="显示名称" validate="required">
-              <p>登录时所用名称，不能重复</p>
-            </div>
-            <div class="form-group">
-              <label>用户名</label>
-              <input type="password" name="username" placeholder="4到20个字符">
+              <input v-model="user.displayName" type="text" placeholder="显示名称" validate="required">
               <p>登录时所用名称，不能重复</p>
             </div>
           </div>
@@ -44,10 +39,42 @@
 </template>
 
 <script>
-  import Top from './Top'
+  import Top from './Top';
+  import store from '../store/index';
 
   export default {
-    name: 'userList'
+    name: 'userList',
+    components: {
+      Top
+    },
+    props: {
+      shouldTipShow: Boolean,
+      type: String,
+      text: String,
+      currentRoute: String
+    },
+    data() {
+      return {
+        user: {},
+        repassword: ''
+      }
+    },
+    methods: {
+      getUser() {
+        store.fetchUser(this).then(result => {
+          this.user = result[0];
+        })
+      },
+      submit() {
+        if (this.user.password !== this.repassword) return;
+        store.patchUser(this, this.user._id, this.user).then(result => {
+
+        });
+      }
+    },
+    created() {
+      this.getUser();
+    }
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">

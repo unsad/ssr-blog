1979115d0b70840d5cd24cb22297380897a4ee57
unsad <wzxaylryw@hotmail.com>
2017-08-03 22:49:53 +0800

    [back-control]更新标签列表组件

--- client_side/back-control/src/components/CateList.vue
+++ client_side/back-control/src/components/CateList.vue
@@ -1,83 +1,60 @@
 <template>
   <div>
-    <div class="manage-container"></div>
+    <div class="manage-container">
     <div>
       <input type="text" placeholder="请输入关键字">
       <i class="search-button"></i>
     </div>
-    <Tabs>
-      <Tab title="全部"></Tab>
-      <Tab title="已发布"></Tab>
-      <Tab title="审核中"></Tab>
-      <Tab title="已拒绝"></Tab>
-    </Tabs>
     <table class="table">
       <thead>
       <tr>
         <th>标题</th>
-        <th>作者</th>
-        <th>状态</th>
-        <th>创建日期</th>
-        <th>修改日期</th>
+        <th>缩略名</th>
+        <th>文章数</th>
+        <th>操作</th>
       </tr>
       </thead>
       <tbody>
       <tr v-if="isLoading">
         <td colSpan="8" class="center">加载中</td>
       </tr>
-      <tr v-if="!items.length">
+      <tr v-if="!cates.length">
         <td colSpan="8" class="center">暂无文章</td>
       </tr>
-      <tr v-else>
+      <tr v-else v-for="cate of cates">
         <td>
-          <Link to={`/post/edit/${item.id}` title="{item.title}">
-          {item.title}
-          </Link>
-          <a href="{/post/${item.pathname}.html" target="_blank">
+          <router-link :to='{name: "editCate", params: {id: cate._id}}' :title="cate.name">
+          {{cate.name}}
+          </router-link>
+          <a v-if="cate.status === 3" :href="`/post/${cate.pathname}.html`" target="_blank">
             <span></span>
           </a>
         </td>
-        <td>{item.user.display_name || item.user.name}</td>
-        <td></td>
-        <td>{!item.create_time || item.create.time === '0000-00-0000:00:00}</td>
-        <td>
-          <button v-if="showPassAndDeny"
-                  type="button"
-                  disabled="{[0, 3].includes(post.status)}"
-          >
-            <span class="ok"></span>
-            通过
-          </button>
-          <span v-if="showPassAndDeny"></span>
-          <button v-if="showPassAndDeny"
-                  type="button"
-                  disabled="{[0, 2].includes([post.status}">
-            <span v-if="showPassAndDeny"></span>
-            拒绝
-          </button>
-          <span v-if="showPassAndDeny"></span>
-          <Link v-if="showEditandDel" to="{`/post/edit/${post.id}`}" title="{post.title}">
+        <td>{{cate.pathName}}</td>
+        <td>0</td>
+        <td><router-link :to="{name: 'editCate', params: {id: cate._id}}">
           <button v-if="showEditAndDel" type="button">
             <span v-if="showEditAndDel"></span>
             编辑
           </button>
-          </Link>
+        </router-link>
+        <span v-if="showEditAndDel"></span>
+        <button
+          v-if="showEditAndDel"
+          type="button" @click="deleteCate(cate._id)">
           <span v-if="showEditAndDel"></span>
-          <button
-            v-if="showEditAndDel"
-            type="button">
-            <span v-if="showEditAndDel"></span>
-            删除
-          </button>
+         <span>删除</span>
+        </button>
         </td>
       </tr>
       </tbody>
     </table>
   </div>
+  </div>
 </template>
-
 <script>
-  import Top from './Top'
+  import Top from './Top';
+  import store from '../store/index';
 
   export default {
     name: 'cateList',
@@ -92,8 +69,26 @@
     },
     data() {
       return {
-
+        cates: [],
+        showPassAndDeng: true,
+        showEditAndDel: true
       }
+    },
+    methods: {
+      getCate() {
+        store.fetchCate(this).then(result => {
+          this.cates = result;
+        });
+      },
+      deleteCate(id) {
+        store.deleteCate(this, id).then(result => {
+          console.log(result);
+          this.cates = this.cates.filter(val => val._id !== id);
+        });
+      }
+    },
+    mounted() {
+      this.getCate();
     }
   }
 </script>

5c65c9d313c44f89194c753f1c96102b12fbc7cb
unsad <wzxaylryw@hotmail.com>
2017-08-12 18:46:58 +0800

    [back]完成jwt权限控制

--- server_side/routes/admin.js
+++ server_side/routes/admin.js
@@ -1,3 +1,53 @@
-/**
- * Created by unsad on 2017/8/11.
- */
+const redis = require('../model/redis');
+const tokenService = require('../service/token');
+
+exports.login = async (next) => {
+  let model = models.user;
+  let error, result;
+  let users, user;
+
+  try {
+    users = await model.find({name: this.request.body.name}).exec();
+    console.log(users);
+    user = {
+      name: users[0].name,
+      password: users[0].password
+    };
+
+    if (user.password === this.request.body.password) {
+      let token = tokenService.createToken(user);
+      redis.set('token', token, 'EX', tokenService.expiresIn, () => {
+
+      });
+      console.log(token);
+      return this.body = {token: token};
+    } else {
+      return this.body = 'Get token fails. Check name and password';
+    }
+  } catch (_error) {
+    error = _error;
+    return this.body = _error;
+  }
+};
+
+exports.logout = async (next) => {
+  const headers = this.request.headers;
+  let token;
+  try {
+    token = headers['authorization'];
+  } catch (err) {
+    return this.body = err;
+  }
+
+  if (!token) {
+    return this.body = 'Token not found';
+  }
+
+  const result = tokenService.verifyToken(token);
+
+  if (result === false) {
+    return this.body = 'Token verify failed';
+  } else {
+    // redis
+  }
+};

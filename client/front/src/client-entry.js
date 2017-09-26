d4d6fc5c1fc0839df1eed43594e399e0f2f12b96
unsad <wzxaylryw@hotmail.com>
2017-09-26 23:28:44 +0800

    [front]vuex数据设计

--- client/front/src/client-entry.js
+++ client/front/src/client-entry.js
@@ -2,6 +2,7 @@
  * Created by unsad on 2017/9/20.
  */
 require('es6-promise').polyfill();
-import { app } from './main';
+import { app, store } from './main';
 
+store.replaceState(window.__INITIAL_STATE__);
 app.$mount('#app');
--- client/front/src/store/store.js
+++ client/front/src/store/store.js
@@ -1,3 +1,41 @@
 /**
  * Created by unsad on 2017/9/25.
  */
+import Vue from 'vue';
+import Vuex from 'vuex';
+import api from './index';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    itemsPerPage: 10,
+    item: []
+  },
+  actions: {
+    FETCH_ITEMS: ({commit, state}, {queryJSON, page}) => {
+      return api.fetchBlogByPage(queryJSON, page).then(items => commit('SET_ITEMS', {items}));
+    },
+    FETCH_USER: ({commit, state}, {id}) => {
+      return state.users[id] ? Promise.resolve(state.users[id]) : fetchUser(id).then(user => commit('SET_USER', { user }))
+    }
+  },
+  mutations: {
+    SET_ITEMS: (state, { items }) => {
+      items.forEach((item, index) => {
+        if (item) {
+          Vue.set(state.items, index, item);
+        }
+      });
+    },
+    SET_USER: (state, { user }) => {
+      Vue.set(state.users, user.id, user);
+    }
+  },
+  getters: {
+    items (state, getters) {
+      const { items, itemsPerPage, lists} = state;
+      return items;
+    }
+  }
+});

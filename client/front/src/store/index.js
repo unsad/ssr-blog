1e6964ac1416f21afd9fc65803fc2019c06b8ac4
unsad <wzxaylryw@hotmail.com>
2017-10-05 10:52:17 +0800

    [front]更新前端接口api

--- client/front/src/store/index.js
+++ client/front/src/store/index.js
@@ -3,195 +3,80 @@
  */
 import axios from 'axios';
 
-const host = typeof location === 'undefined' ? 'http://localhost:8080' : '';
+const host = typeof location === 'undefined' ?
+              process.env.NODE_ENV === 'production' ?
+                'http://localhost:3000' :
+                  'http://localhost:8080/proxyPrefix' : '/proxyPrefix';
 
-const aboutAPI = `/proxyPrefix/api/post/57dbe47c2993f70dc6d6b12c`;
-const blogAPI = `${host}/proxyPrefix/api/post`;
-const tagAPI = `${host}/proxyPrefix/api/tag`;
-const postTagAPI = `${host}/proxyPrefix/api/postTag`;
-const postCateAPI = `${host}/proxyPrefix/api/postCategory`;
-const categoryAPI = `${host}/proxyPrefix/api/category`;
+const prefix = `${host}/api`;
 
-const root = '/proxyPrefix/api';
-
+const aboutAPI = `${host}/api/post?title=关于`;
+const blogAPI = `${host}/api/post`;
+const tagAPI = `${host}/api/tag`;
+const postTagAPI = `${host}/api/postTag`;
+const postCateAPI = `${host}/api/postCategory`;
+const categoryAPI = `${host}/api/category`;
 const perPage = 10;
 
 const store = {};
 
 export default store;
 
-store.login = (conditions, args) => {
-  return axios.post(`/proxyPrefix/admin/login`, conditions);
-};
+function isObject(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1) === 'Object';
+}
 
-store.logout = (conditions, args) => {
-  return axios.post(`/proxyPrefix/admin/logout`, conditions);
-};
-
-store.fetchUpdate = (conditions = {}, args) => {
-  return axios.get(`/proxyPrefix/api/update`, conditions);
-};
-
-store.deleteUpdate = id => {
-  return axios.delete(`/proxyPrefix/api/update/${id}`);
-};
+function convertObjectToArray(args) {
+  return isObject(args) ? Object.keys(args).map((value, index) => {
+    let temp = {};
+    temp[value] = args[value];
+    return temp;
+  }) : [];
+}
 
 // post CRUD
 
-store.fetchBlog = (conditions, args) => {
-  return axios.get(blogAPI + `?conditions=${JSON.stringify(conditions)}&sort=1`).then(response => response.data, err => console.log(err));
+store.fetchPost = (conditions, args) => {
+  let target = `${blogAPI}/?conditions=${JSON.stringify(conditions)}`;
+  if (args.select) {
+    target += `&select=${JSON.stringify(args.select)}`;
+    delete args.select;
+  }
+  args = convertObjectToArray(args);
+  return args.reduce((prev, curr) => {
+    prev = prev.query(curr);
+    return prev;
+  }, axios.get(target)).then(response => response.body, err => console.log(err));
 };
 
-store.fetchBlogByID = id => {
-  return axios.get(blogAPI + `/${id}`).then(response => response.data, err => console.log(err));
-};
-
-store.newBlog = json => {
-  return axios.post(blogAPI, json).then(response => response.data, err => console.log(err));
-};
-
-store.patchBlog = (id, json) => {
-  return axios.patch(`${blogAPI}/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-store.deleteBlogByID = (id, page = 0) => {
-  return axios.delete(`${blogAPI}/${id}`).then(response => response.data, err => console.log(err));
+store.fetchPostByID = (id, conditions, args) => {
+  let target = `${blogAPI}/${id}?conditions=${JSON.stringify(conditions)}`;
+  if (args.select) {
+    target += `&select=${JSON.stringify(args.select)}`;
+    delete args.select;
+  }
+  args = convertObjectToArray(args);
+  return args.reduce((prev, curr) => {
+    prev = prev.query(curr);
+    return prev;
+  }, axios.get(target)).then(response => response.body, err => console.log(err));
 };
 
 // cate CRUD
 
-store.fetchCate = (conditions = {}, arg) => {
-  return axios.get(`${root}/category?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
-};
-
-store.newCate = name => {
-  if (typeof name === 'undefined' || name === '') return;
-  return axios.post(`${root}/category`, {
-    name
-  }).then(response => response.data, err => console.log(err));
-};
-
-store.fetchCateById = id => {
-  return axios.get(`${root}/category/${id}`).then(response => response.data, err => console.log(err));
-};
-
-store.patchCate = (id, json) => {
-  return axios.patch(`${root}/category/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-store.deleteCate = id => {
-  return axios.delete(`${root}/category/${id}`).then(response => response.data, err => console.log(err));
-};
-
 // tag CRUD
 
-store.fetchTag = (conditions = {}, args) => {
-  return axios.get(`${root}/tag?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
+store.fetchTags = () => {
+  return axios.get(tagAPI).then(response => response.body, err => console.log(err));
 };
 
-store.fetchTagById = id => {
-  return axios.get(`${root}/tag/${id}`).then(response => response.data, err => console.log(err));
-};
-
-store.newTag = name => {
-  if (typeof name === 'undefined' || name === '') return;
-  return axios.post(`${root}/tag`, {
-    name
-  }).then(response => response.data, err => console.log(err));
-};
-
-store.patchTag = (id, json) => {
-  return axios.patch(`${root}/tag/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-store.deleteTag = id => {
-  return axios.delete(`${root}/tag/${id}`).then(response => response.data, err => console.log(err));
-};
-
-// version CRUD
-
-store.fetchVersion = (conditions = {}, args) => {
-  return axios.get(`${root}/update?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
-};
-
-store.fetchVersionById = id => {
-  return axios.get(`${root}/update/${id}`).then(response => response.data, err => console.log(err));
-};
-
-store.newVersion = json => {
-  return axios.post(`${root}/update`, json).then(response => response.data, err => console.log(err));
-};
-
-store.patchVersion = (id, json) => {
-  return axios.patch(`${root}/update/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-store.deleteVersion = id => {
-  return axios.delete(`${root}/update/${id}`).then(response => response.data, err => console.log(err));
+store.fetchPostTags = () => {
+  return axios.get(postTagAPI).then(response => response.body, err => console.log(err));
 };
 
 // option CRUD
 
-store.fetchOption = (conditions = {}, args) => {
-  return axios.get(`${root}/option?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
+store.fetchOption = () => {
+  return axios.get(`${prefix}/option?select={"key": 1, "value": 1}`).then(response => response.body, err => console.log(err));
 };
 
-store.patchOption = (id, json) => {
-  return axios.patch(`${root}/option/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-// user CRUD
-
-store.fetchUser = () => {
-  return axios.get(`${root}/user`).then(response => response.data, err => console.log(err));
-};
-
-store.patchUser = (id, json) => {
-  return axios.patch(`${root}/user/${id}`, json).then(response => response.data, err => console.log(err));
-};
-
-// many to many
-
-store.deleteTagsByPostID = id => {
-  return axios.delete(`${postTagAPI}/${id}`).then(response => response.data, err => console.log(err));
-};
-
-store.deleteCateByPostID = id => {
-  return axios.delete(`${postCateAPI}/${id}`).then(response => response.data, err => console.log(err));
-};
-
-// postTag
-
-store.fetchPostTags = () => {
-  return axios.get(postTagAPI).then(response => response.data, err => console.log(err));
-};
-
-store.fetchPostTagsByID = (conditions) => {
-  return axios.get(postTagAPI + `?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
-};
-
-// postCate
-
-store.fetchPostCate = () => {
-  return axios.get(postCateAPI).then(response => response.data, err => console.log(err));
-};
-
-store.fetchPostCateByID = (conditions) => {
-  return axios.get(postCateAPI + `?conditions=${JSON.stringify(conditions)}`).then(response => response.data, err => console.log(err));
-};
-
-store.deletePostTags = id => {
-  return axios.delete(`${postTagAPI}/${id}`).then(response => response, err => console.log(err));
-};
-
-store.addPostTags = json => {
-  return axios.post(`${postTagAPI}`, json).then(response => response.data, err => console.log(err));
-};
-
-store.deletePostCates = id => {
-  return axios.delete(`${postCateAPI}/${id}`).then(response => response, err => console.log(err));
-};
-
-store.addPostCates = (vue, json) => {
-  return axios.post(`${postCateAPI}`, json).then(response => response.data, err => console.log(err));
-};

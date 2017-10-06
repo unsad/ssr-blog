d5df5d47a6449600d8796168cb6bc0ed77d2c9aa
unsad <wzxaylryw@hotmail.com>
2017-10-06 11:05:47 +0800

    [front]更新入口文件

--- client/front/src/client-entry.js
+++ client/front/src/client-entry.js
@@ -1,7 +1,45 @@
 /**
  * Created by unsad on 2017/9/20.
  */
-import { app, store } from './main';
+import Vue from 'vue';
+import { createApp } from './main';
 
-store.replaceState(window.__INITIAL_STATE__);
-app.$mount('#app');
+Vue.mixin({
+  beforeRouteUpdate(to, from, next) {
+    const { asyncData } = this.$options;
+    if (asyncData) {
+      asyncData({
+        store: this.$store,
+        route: to
+      }).then(next).catch(next);
+    } else {
+      next();
+    }
+  }
+});
+
+const {app, router, store} = createApp();
+
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__);
+}
+
+router.onReady(() => {
+  router.beforeResolve((to, from, next) => {});
+  const matched = router.getMatchedComponents(to);
+  const prevMatched = router.getMatchedComponents(from);
+  let diffed = false;
+  const activated = matched.filter((c, i) => {
+    return diffed || (diffed = (prevMatched[i] !== c));
+  });
+  const asyncDataHooks = activated.map(c => c.asyncData).filter(_ => _);
+  if (!asyncDataHooks.length) return next();
+
+  Promise.all(asyncDataHooks.map(hook => hook({store, route: to}))).then(next).catch(next);
+
+  app.$mount('#app');
+});
+
+if ('https:' === location.protocol && navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
--- client/front/src/server-entry.js
+++ client/front/src/server-entry.js
@@ -1,22 +1,35 @@
 /**
  * Created by unsad on 2017/9/20.
  */
-import { app, router, store } from './main';
+import { createApp } from './main';
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 export default context => {
-  router.push(context.url);
+  return new Promise((resolve, reject) => {
+    const s = isDev && Date.now();
+    const {app, router, store} = createApp();
+    const {url} = context;
+    const {fullPath} = router.resolve(url).route;
 
-  const s = isDev && Date.now();
-
-  return Promise.all(router.getMatchedComponents().map(component => {
-    if (component.preFetch) {
-      return component.preFetch(store, context);
+    if (fullPath !== url) {
+      return reject({url: fullPath});
     }
-  })).then(() => {
-    isDev && console.log(`data pre-fetch: ${Date.now() - s}ms`);
-    context.initialState = store.state;
-    return app;
+
+    router.push(url);
+
+    router.onReady(() => {
+      const matchedComponents = router.getMatchedComponents();
+      if (!matchedComponents.length) return reject({code: 404});
+
+      Promise.all(matchedComponents.map(({asyncData}) => asyncData && asyncData({
+        store,
+        route: router.currentRoute
+      }))).then(() => {
+        isDev && console.log(`data pre-fetch: ${Date.now() - s}ms`);
+        context.state = store.state;
+        resolve(app);
+      }).catch(reject);
+    }, reject);
   });
 }

b04ab5bdfe8d648e6d68a1bb4134cb02635d8fc5
unsad <wzxaylryw@hotmail.com>
2017-10-12 00:42:45 +0800

    [back-control]更新核心组件

---
+++ client/element/src/components/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dashboard">
+    <top></top>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+  import Api from '../store/api'
+  import Sidebar from './Sidebar'
+  import Top from './Top'
+
+  export default {
+    name: 'dashboard',
+    data() {
+      return {
+        title: ''
+      }
+    },
+    components: {
+      sidebar,
+      top
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ client/element/src/components/Logout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import Api from '../store/api';
+
+  export default {
+    name: 'logout',
+    methods: {
+      logout() {
+        Api.logout().then(response => {
+          localStorage.removeItem('token');
+          localStorage.removeItem('username');
+          this.$notify({
+            title: '成功',
+            message: '登出成功',
+            type: 'success',
+            duration: 2000
+          });
+          this.$router.push({path: '/admin/login'});
+        })
+      }
+    },
+    beforeMount() {
+      this.logout();
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>

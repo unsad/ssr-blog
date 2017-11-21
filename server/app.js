77c0e7b80f27ac2b2261a6990590c61c4e3cc99b
unsad <wzxaylryw@hotmail.com>
2017-11-21 22:49:42 +0800

    [back] 防止cc攻击

--- server/app.js
+++ server/app.js
@@ -12,8 +12,17 @@ const models = require('./model/mongo');
 const redis = require('./model/redis');
 const config = require('./conf/config');
 const plugins = require('./plugins');
+const ratelimit = require('koa-ratelimit');
 
 const app = new Koa();
+
+app.use(ratelimit({
+  db: redis,
+  duration: 1000,
+  errorMessage: 'Slow Down Your Request',
+  id: ctx => ctx.ip,
+  max: 10
+}));
 
 app.use(bodyParser());
 

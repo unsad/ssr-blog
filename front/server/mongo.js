8934ea3adb4dec96d94249a74a88556b1d16d785
unsad <wzxaylryw@hotmail.com>
2018-02-13 13:07:07 +0800

    更新监听端口和部署路径

--- front/server/mongo.js
+++ front/server/mongo.js
@@ -1,7 +1,7 @@
 const env = process.env;
 
 module.exports = {
-  ssrPort: env.ssrPost || 8080,
+  ssrPort: env.ssrPost || 8081,
   serverPort: env.serverPort || 3000,
   redisPassword: env.redisPassword || '',
   enableServerSideGA: env.enableServerSideGA || false

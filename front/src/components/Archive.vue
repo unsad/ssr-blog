35d82f8af03d3eeb02d6d4b8ca5801569beb3ba2
unsad <wzxaylryw@hotmail.com>
2018-02-28 22:15:12 +0800

    [front] 更新归档样式

--- front/src/components/Archive.vue
+++ front/src/components/Archive.vue
@@ -2,14 +2,16 @@
   <div id='main'>
     <article class="post archive">
       <h1 class="title">{{title}}</h1>
-      <div class="entry-content" v-for="(item, key) of achieves" :key="key">
-        <h3>{{key}} ({{item.length}})</h3>
-        <ul>
-          <li v-for="subItem of item" :key="subItem.title">
-            <router-link :to="{name: 'post', params: {pathName: subItem.pathName}}" :title="subItem.title">{{subItem.title}}</router-link>&nbsp;
-            <span class="date">{{subItem.createdAt.split(' ')[0]}}</span>
-          </li>
-        </ul>
+      <div class="timeline" v-for="(item, key) of achieves" :key="key">
+        <div class="content">
+          <h3>{{key}} ({{item.length}})</h3>
+          <ul>
+            <li v-for="subItem of item" :key="subItem.title">
+              <router-link :to="{name: 'post', params: {pathName: subItem.pathName}}" :title="subItem.title">{{subItem.title}}</router-link>&nbsp;
+              <span class="date">{{subItem.createdAt.split(' ')[0]}}</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </article>
     <my-footer></my-footer>
@@ -68,6 +70,44 @@
   }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
-#main 
+#main  
   padding: 1rem
+  .timeline
+    position: relative
+    width: 0.5rem
+    margin: 0 auto 
+    background: #000
+    &:before
+      content: '';
+      background: red;
+      position: absolute;
+      left: 50%;
+      top: 0;
+      transform: translateX(-50%);
+      width: 2rem;
+      height: 2rem;
+      border: 3px solid #000;
+      border-radius: 50%;
+    .content 
+      background: #000
+      position: relative 
+      width: 40rem
+      padding: 2rem
+      top: 0.5rem
+      &:before 
+        content: ''
+        background: #000 
+        position: absolute 
+        top: 0
+        width: 3rem 
+        height: 0.3rem 
+    &:nth-child(odd) .content
+      left: 4rem 
+      background: #000 
+      &:before 
+        left: -3rem
+    &:nth-child(even) .content
+      left: calc(-40rem - 4rem)
+      &:before 
+        right: -3rem
 </style>

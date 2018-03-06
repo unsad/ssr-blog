99209649805f1f93bb944be773b5a4920cbbe872
unsad <wzxaylryw@hotmail.com>
2018-03-06 22:41:37 +0800

    [front] 完成标签展示hardcode

--- front/src/components/About.vue
+++ front/src/components/About.vue
@@ -13,7 +13,84 @@
                   stroke="black" fill="transparent" stroke-width="6"/>
             </svg>
             <span class="direct-content">节能主义</span>
-        </div>
+          </div>
+          <div class="direct-box box-2">
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,0 100,75 200,75"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+            <span class="direct-content">INFJ</span>
+          </div>
+           <div class="direct-box box-3">
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,180 100,75 200,75"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+            <span class="direct-content">前端开发</span>
+          </div>
+          <div class="direct-box box-4">
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,180 100,75 200,75"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+            <span class="direct-content">人像摄影</span>
+          </div>
+          <div class="direct-box box-5">
+            <span class="direct-content">手办</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,20"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
+           <div class="direct-box box-6">
+            <span class="direct-content">Motion Graphic</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,200"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
+          <div class="direct-box box-7">
+            <span class="direct-content">动画</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,200"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
+          <div class="direct-box box-8">
+            <span class="direct-content">主机游戏</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,20"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
+          <div class="direct-box box-10">
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,180 100,75 200,75"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+            <span class="direct-content">电子乐</span>
+          </div>
+           <div class="direct-box box-9">
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,0 100,75 200,75"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+            <span class="direct-content">剪辑</span>
+          </div>
+          <div class="direct-box box-12">
+            <span class="direct-content">影视后期</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,20"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
+          <div class="direct-box box-11">
+            <span class="direct-content">爵士乐</span>
+            <svg class="direct-line" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polyline points="0,75 100,75 200,20"
+                  stroke="black" fill="transparent" stroke-width="6"/>
+            </svg>
+          </div>
         </div>
       </div>
     <my-footer></my-footer>
@@ -49,6 +126,9 @@ export default {
     position: relative
     width: 500px
     display: inline-block
+    object  
+      z-index: 1
+      position: relative
   .direct-box 
     position: absolute
     display: flex 
@@ -60,7 +140,41 @@ export default {
       font-size: 2rem
       padding: 0.5rem
       two-color-border(0.5rem, 0.2rem)
+
   .box-1 
     left: 70%
     bottom: 4rem
+  .box-2 
+    left: 65% 
+    bottom: 9rem
+  .box-3
+    left: 70%
+    top: 3rem
+  .box-4
+    left: 50%
+    top: 8rem
+  .box-5 
+    left: -20%
+    bottom: 0
+  .box-6
+    left: -30%
+    top: 9rem
+  .box-7 
+    left: -10%
+    top: 3rem
+  .box-8
+    left: -35%
+    bottom: 5rem
+  .box-9
+    left: 50%
+    top: 17rem
+  .box-10
+    left: 68%
+    top: 16rem
+  .box-11
+    left: -5%
+    top: 15rem
+  .box-12
+    left: -30% 
+    bottom: 18rem
 </style>

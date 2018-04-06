c1ea86cec059e7fd8b3724fde51b9d4750530eef
unsad <wzxaylryw@hotmail.com>
2018-04-06 16:19:37 +0800

    [front] 完成catchme背景

--- front/src/components/CatchMe.vue
+++ front/src/components/CatchMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="catchme">
      CatchMe
   </div>
 </template>
@@ -14,5 +14,13 @@ export default {
 };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
- 
+    #catchme 
+        background: url(../../static/catchme.svg) no-repeat left bottom/62vh,
+                    linear-gradient(145deg, transparent, transparent calc(80vw + 1rem), red 0),
+                    linear-gradient(145deg, transparent, transparent 80vw, white 0),
+                    linear-gradient(130deg, transparent, transparent calc(20vh + 1rem), transparent 0),
+                    linear-gradient(130deg, black, black 20vh, transparent 0),
+                    linear-gradient(110deg, transparent, transparent calc(45vh + 1rem), black 0),
+                    linear-gradient(110deg, red, red 45vh, white 0)
+                    
 </style>

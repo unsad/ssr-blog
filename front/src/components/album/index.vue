8db44140a09127c0ecdcf4166e897242faaffad6
unsad <wzxaylryw@hotmail.com>
2018-08-05 23:00:15 +0800

    [front] 初始化相册组件

---
+++ front/src/components/album/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Album extends Vue {
+ 
+}
+</script>
+<style module="album" lang="stylus" rel="stylesheet/stylus">
+
+</style>

966dc1bfa55e53b33699411facbaf13518338981
unsad <wzxaylryw@hotmail.com>
2018-08-21 22:47:44 +0800

    [部署] 更新服务端构建相关包

--- front/build/webpack.server.config.js
+++ front/build/webpack.server.config.js
@@ -43,7 +43,8 @@ module.exports = merge(baseConfig, {
     new VueSSRServerPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      'process.env.VUE_ENV': '"server"'
+      'process.env.VUE_ENV': '"server"',
+      'process.env.serverHost': JSON.stringify(process.env.serverHost || 'localhost')
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
--- front/src/store/create-api-server.js
+++ front/src/store/create-api-server.js
@@ -2,8 +2,7 @@
  * Created by unsad on 2017/11/15.
  */
 const isProd = process.env.NODE_ENV === 'production';
-const { serverPort, serverHost } = require('../../server/config');
 
 export default {
-  host: isProd ? `http://${serverHost}:${serverPort}` : 'http://localhost:8080/proxyPrefix'
+  host: isProd ? `http://${process.env.serverHost}:3000` : 'http://localhost:8080/proxyPrefix'
 };

38534852bd0ce654e9881acd44d6bbd07b963b85
unsad <wzxaylryw@hotmail.com>
2018-09-02 15:07:24 +0800

    [front] 更新album翻页逻辑

--- front/src/components/album/index.vue
+++ front/src/components/album/index.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-      <div :class="albumCss.selectedBox" v-show="selectedUrl">
-        <img :class="albumCss.selectedContent" :src="selectedUrl" @click="selectedUrl = ''"/>
+      <div :class="albumCss.selectedBox" v-show="selectedIndex >= 0">
+        <span @click="front">front</span>
+        <img :class="albumCss.selectedContent" :src="selectedIndex >= 0 ? album[selectedIndex].url : ''" @click="selectedIndex = -1"/>
+        <span @click="next">next</span>
       </div>
-      <img v-for="item of album" :key="item.url" :src="getPreviewUrl(item.url, 200)" @click="selectedUrl = item.url"/>
+      <img v-for="(item, index) of album" :key="item.url" :src="getPreviewUrl(item.url, 200)" @click="selectedIndex = index"/>
     </div>
 </template>
 
@@ -21,22 +23,32 @@ import {
   }
 })
 export default class Album extends Vue {
-  selectedUrl: string = '';
+  selectedIndex: number = -1;
   @Getter album
   getPreviewUrl(url, width) {
     return `${url}?imageView2/2/w/${width}`;
+  }
+  next() {
+    this.selectedIndex = (this.selectedIndex + 1) % this.album.length;
+  }
+  front() {
+    this.selectedIndex = this.selectedIndex - 1 < 0 ? this.album.length - 1 : this.selectedIndex - 1;
   }
 }
 </script>
 <style module="albumCss" lang="stylus" rel="stylesheet/stylus">
 .selectedBox
   position: absolute
+  display: flex
+  align-items: center
+  justify-content: center
   top: 0
   bottom: 0
   left: 0
   right: 0
   background: $main-second-color
+  color: $main-third-color
   .selectedContent
-    width: 80%
+    width: 90%
     height: auto
 </style>
